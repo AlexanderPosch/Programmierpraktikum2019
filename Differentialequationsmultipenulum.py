14ac883e29808f0326745b6726c552ab979fd7c3
AlexanderPosch <49498206+AlexanderPosch@users.noreply.github.com>
2019-04-17 13:31:16 +0200

    Update Differentialequationsmultipenulum.py

--- Differentialequationsmultipenulum.py
+++ Differentialequationsmultipenulum.py
@@ -1,7 +1,7 @@
+import sys
+sys.path.append('D:\Python\site-packages')
 import sympy as sy
-import numpy as np
-from sympy import sin,cos
-
+print("Import done")
 n=1 #Number of Pendulums-1
 g=9.8 #Acceleration of Gravity
 
@@ -16,54 +16,42 @@ t=sy.symbols('t')
 
 #The differential Equations of a Multipendulum can be calulated using the Lagrange Formalism (In short it states that d/dt*(dL/dy')-dL/dy=0)
 #L=T-V where T is Kinetik Energy, V is Potential Energy. For more information see: https://de.wikipedia.org/wiki/Multipendel
-V=g*sum(map(lambda k: m[k]*sum(map(lambda i:-l[i]*sy.cos(y[i]),[0,k])),[0,n]))
+V=g*sum(map(lambda k: m[k]*sum(map(lambda i:-l[i]*sy.cos(y[i]),list(range(k+1)))),list(range(n+1))))
 def ugly1(k):
-    return sum(map(lambda i: l[i]*v[i]*sy.cos(y[i]),[0,k]))
+    return sum(map(lambda i: l[i]*v[i]*sy.cos(y[i]),list(range(k+1))))
 def ugly2(k):
-    return sum(map(lambda i: l[i]*v[i]*sy.sin(y[i]),[0,k]))
-T=1/2*sum(map(lambda k: m[k]*(ugly1(k)**2+ugly2(k)**2),[0,n]))
+    return sum(map(lambda i: l[i]*v[i]*sy.sin(y[i]),list(range(k+1))))
+T=1/2*sum(map(lambda k: m[k]*(ugly1(k)**2+ugly2(k)**2),list(range(n+1))))
 L=T-V 
 
 #Differentiating L as nessersary for the Lagrangeformalism
-Ldy = np.array([*map(lambda x:L.diff(x),y)])
-Tdv = np.array([*map(lambda x:T.diff(x),v)]) #Since V is indipentent of the v's, only T has to be differentiated
+Ldy = sy.Matrix([*map(lambda x:L.diff(x),y)])
+Tdv = sy.Matrix([*map(lambda x:T.diff(x),v)]) #Since V is indipentent of the v's, only T has to be differentiated
 
 
 #Substituting variables with Derivatives sothat I can differentiate Tdv by t
-for i in range(len(Tdv)):
-    s=Tdv[i]
-    #v=y'(t)
-    s=s.subs(list(map(lambda k:(v[k],y[k](t).diff(t)),range(n+1))))
-    #a=y''(t)
-    s=s.subs(list(map(lambda k:(a[k],y[k](t).diff(t,t)),range(n+1))))
-    #y=y(t)
-    s=s.subs(list(map(lambda k:(y[k],y[k](t)),range(n+1))))
-    Tdv[i]=s
+Tdv=Tdv.subs(list(map(lambda k:(v[k],y[k](t).diff(t)),range(n+1)))) #v=y'(t)
+Tdv=Tdv.subs(list(map(lambda k:(a[k],y[k](t).diff(t,t)),range(n+1)))) #a=y''(t)
+Tdv=Tdv.subs(list(map(lambda k:(y[k],y[k](t)),range(n+1)))) #y=y(t)
 
 #Differentiate Tdv by t
-Tdvdt=np.array([*map(lambda x:x.diff(t),Tdv)])
+Tdvdt=sy.Matrix([*map(lambda x:x.diff(t),Tdv)])
+print(Tdvdt)
+Lagrangev=Tdvdt-Ldy #This is Matrix holds a list of n+1 linear equations in a0,a1,a2,... (here still in the form of second derivatives)
 
-Lagrangev=Tdvdt-Ldy #This is Matrix holds a list of n+1 linear equations in a0,a1,a2
+#Substituting Derivatives with Variables for clarity
+Lagrangev=Lagrangev.subs(list(map(lambda k:(y[k](t).diff(t,t),a[k]),range(n+1)))) #y''(t)=a   
+Lagrangev=Lagrangev.subs(list(map(lambda k:(y[k](t).diff(t),v[k]),range(n+1)))) #y'(t)=v
+Lagrangev=Lagrangev.subs(list(map(lambda k:(y[k](t),y[k]),range(n+1)))) #y(t)=y
 
-#Substituting Derivatives back into Variables 
-for i in range(len(Lagrangev)):
-    s=Lagrangev[i]
-    #y''(t)=a    
-    s=s.subs(list(map(lambda k:(y[k](t).diff(t,t),a[k]),range(n+1))))
-    #y'(t)=v
-    s=s.subs(list(map(lambda k:(y[k](t).diff(t),v[k]),range(n+1))))
-    #y(t)=y
-    s=s.subs(list(map(lambda k:(y[k](t),y[k]),range(n+1))))
-    Lagrangev[i]=s
-    
-
-###This part takes ages but technically this function only has to be called once anyway and having a nice form speeds up fruther computation
+###This part takes ages but technically this function only has to be called once anyway and having a nice form speeds up further computation
 print("This part takes ages but technically this function only has to be called once anyway and having a nice form speeds up fruther computation")
-Lagrangev=sy.simplify(sy.Matrix(Lagrangev))
+Lagrangev=sy.simplify(Lagrangev)
+
 #Lagrangev holds a list of n+1 (in regard to a) linear equations.
 #If we substitute numerical constants in for l,m,v,y they can be solved quite quickly.
 
 #Saving Lagrangev to file 
-f = open('eqations'+str(n+1)+'.py', 'w' )
+f = open('eqations'+str(n+1)+'.txt', 'w' )
 f.write(str(Lagrangev))
 f.close()

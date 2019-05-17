17054c8f562f95d36638ee8a29ad77988bffcdf5
AlexanderPosch <49498206+AlexanderPosch@users.noreply.github.com>
2019-05-17 17:10:49 +0200

    Update minessweepstart2.py

--- minessweepstart2.py
+++ minessweepstart2.py
@@ -15,19 +15,16 @@ class Minesweeper:
     def __init__(self,fsize,bombs,pos=None):
         self.fsize=np.array(fsize)
         self.Mf=np.zeros(fsize, dtype=int)
-        #Array with 0,1,..8 indicating neibouring bombs, 9 means bomb
-        ##### 1-. means flaged
-        ##### 2-. means visble
-        self.Mf,self.Bl=self.bombplacment(bombs)
+        #Array with intergers the first digit denotes the number of neighbouring bombs where 9 denotes that thes a bomb at the location
+        #The second digit denotes whether a tile is 1. Flaged or 2. uncovered.
+        
+        self.Mf, self.Bl = self.bombplacment(bombs)
         self.nnb()
         
-        
-
     def bombplacment(self,bombs):
         c=self.Mf.reshape(self.Mf.size)
         bombpositions=random.sample(range(self.Mf.size),bombs)
-        for i in bombpositions:
-            c[i]=9
+        c[bombpositions]=9
         c=c.reshape(self.fsize)
         return c,list(zip(*np.where(c==9)))
         
@@ -43,31 +40,35 @@ class Minesweeper:
                             self.Mf[tuple(np.array(i)+np.array((j,k)))]+=1
                      
     def flag(self,pos):
-        if Minefield[pos]<10:
-            Minefield[pos]+=10
-        elif Minefield[pos]<20:
-            Minefield[pos]-=10
+        if self.Mf[pos]<10:
+            self.Mf[pos]+=10
+        elif self.Mf[pos]<20:
+            self.Mf[pos]-=10
 
 
         
-    def open(self,pos):
+    def openup(self,pos):
         if self.Mf[pos]<10:
             if self.Mf[pos]==9:
-                fail()
+                a.fail()
             else:
                 self.Mf[pos]+=20
-                floodfill()
+                a.floodfill()
 
             
     def floodfill(self):# Maybee make more fast
+        repeat=True
         while repeat:
             repeat=False
             for i in range(self.fsize[0]):
                 for j in range(self.fsize[1]):
-                    if Minefield[i,j]==20:
+                    if self.Mf[i,j]==20:
                         for k in [-1,0,1]:
                             for l in [-1,0,1]:
                                 if ((i+k>=0)and(j+l)>=0)and ((i+k<self.fsize[0])and(j+l<self.fsize[1])):
-                                    if Minefield[i+k,j+l]<20:
-                                        Minefield[i+k,j+l]+=20
+                                    if self.Mf[i+k,j+l]<20:
+                                        self.Mf[i+k,j+l]+=20
                                         repeat=True
+    def fail(self):
+        print("........Fail...........")
+        globals()['done'] = True
